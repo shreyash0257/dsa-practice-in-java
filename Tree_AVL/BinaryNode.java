5b9d472f320ce41842a0ee5deaa5c89d6312ed51
shreyash0257 <98897735+shreyash0257@users.noreply.github.com>
2023-08-11 08:58:22 +0530

    Add files via upload

---
+++ Tree_AVL/BinaryNode.java
@@ -0,0 +1,14 @@
+package Tree_AVL;
+
+public class BinaryNode {
+    public int value;
+    public int height;
+    public BinaryNode left;
+    public BinaryNode right;
+
+    public BinaryNode() {
+        height = 0;
+    }
+
+
+}
---
+++ Tree_AVL/Main.java
@@ -0,0 +1,17 @@
+package Tree_AVL;
+
+public class Main {
+    public static void main(String[] args) {
+        AVL avl = new AVL();
+        avl.insert(5);
+        avl.insert(10);
+        avl.insert(15);
+        avl.insert(20);
+        avl.levelOrder();
+        avl.delete(5);
+        System.out.println();
+        avl.levelOrder();
+        System.out.println();
+        avl.deleteAVLTree();
+    }
+}
